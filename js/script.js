29580202810a61f7a6c1ad6b85a892e800836b4a
Tanzir Rifat <93050315+tanzirrahmanrifat@users.noreply.github.com>
2022-04-04 00:15:38 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+//  scrolltop 
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 70){
+            $("#topbtn").fadeIn();
+        } else{
+           $("#topbtn").fadeOut();
+        }
+        
+    });
+
+    $("#topbtn").click(function(){
+        $('html ,body').animate({
+            scrollTop : 0},800)
+    })
+})
